79375fba6c12f790962cfc9af0b4153db818cf1e
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-10-07 11:09:36 +0300

    Support append mode in pack.c

--- pack/pack.c
+++ pack/pack.c
@@ -20,6 +20,15 @@ void append(char *file, int pack) {
 	unlink(file);
 }
 
+off_t get_offset(int fp) {
+	struct file f;
+
+	lseek(fp, - sizeof(f), SEEK_END);
+	read(fp, &f, sizeof(f));
+
+	return f.begin + f.size;
+}
+
 int main(int argc, char **argv) {
 	struct stat st;
 	struct dirent *de;
@@ -47,17 +56,18 @@ int main(int argc, char **argv) {
 	snprintf(idx_path, sizeof(idx_path), "%s/%s", argv[1], idx_name);
 	snprintf(pack_path, sizeof(idx_path), "%s/%s", argv[1], pack_name);
 
-	unlink(idx_path);
-
-	if ((idx = open(idx_path, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == EOF)
-		usage(argv[0]);
-
-	unlink(pack_path);
-
-	if ((pack = open(pack_path, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == EOF)
-		usage(argv[0]);
-
 	memset(&f, '\0', sizeof(f));
+
+	if ((idx = open(idx_path, O_APPEND | O_RDWR)) == EOF) {
+		if ((idx = open(idx_path, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == EOF) {
+			usage(argv[0]);
+		}
+	} else
+		f.begin = get_offset(idx);
+
+	if ((pack = open(pack_path, O_APPEND | O_WRONLY)) == EOF)
+		if ((pack = open(pack_path, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == EOF)
+			usage(argv[0]);
 
 	while ((de = readdir(d))) {
 		if (strstr(de->d_name, ".idx") || strstr(de->d_name, ".pack"))
