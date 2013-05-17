98e2a8bd71f72bf6e610d6cae4d5d03c0253f29a
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2013-05-17 10:05:00 +0300

    Add video-gallery.php

---
+++ video-gallery.php
@@ -0,0 +1,95 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Welcome to Web Server</title>
+<script type="text/javascript" src="/jwplayer/jwplayer.js"></script>
+</head>
+<body bgcolor="white" text="black">
+<table>
+<?php
+$id = 1;
+
+function dirent($dir) {
+    $ret = array();
+
+    if ($d = opendir($dir)) {
+	while (($e = readdir($d)) !== false) {
+	    if ($e == '.' || $e == '..' || $e == 'images')
+		continue;
+
+	    if (is_file("$dir/$e") && (($dot = strrchr($e, '.')) !== false)) {
+		$ext = substr($dot, 1);
+
+		switch ($ext) {
+		    case 'flv':
+		    case 'mp4':
+		    case 'm4v':
+		    case 'f4v':
+		    case 'mov':
+			break;
+		    default:
+			continue 2;
+		}
+	    }
+	    $ret[] = $e;
+	}
+    }
+
+    sort($ret);
+
+    return($ret);
+}
+
+function show_next($dir, $files) {
+    global $id;
+
+    if (($first = array_shift($files)) !== NULL) {
+	$img = substr($first, 0, strrpos($first, '.')) . '.jpg';
+
+	if (!file_exists("$dir/images/$img")) {
+	    system("ffmpeg -y -i \"$dir/$first\" -ss 15 -vframes 1 -an -sameq -f image2 \"$dir/images/$img\"");
+	}
+
+	echo <<<END
+<div id="clip$id">Loading the player...</div>
+<script type="text/javascript">
+    jwplayer("clip$id").setup({
+	height: 300,
+	width: 480,
+	file: "$dir/$first",
+	image: "$dir/images/$img"
+    });
+</script>
+END;
+	$id++;
+    } else {
+	echo '&nbsp;';
+    }
+}
+
+if (isset($_GET['d'])) {
+    $dir = urldecode($_GET['d']);
+    $files = dirent("lf/$dir");
+
+    $rows = ceil(count($files) / 2);
+
+    for ($i = 0; $i < $rows; $i++) {
+	echo '<tr><td>';
+	show_next("lf/$dir", &$files);
+	echo '</td><td>';
+	show_next("lf/$dir", &$files);
+	echo '</td></tr>';
+    }
+} else {
+    $dirs = dirent("lf");
+
+    foreach ($dirs as $dir) {
+	$count = count(dirent("lf/$dir"));
+
+        print "<tr><td><a href='?d=" . urlencode($dir) . "'>$dir</a></td><td align='right'>$count</td></tr>\n";
+    }
+}
+?>
+</table>
+</body>
+</html>
